a048ef30f434d1b9696312010663209d68c1f03b
Carlos Robinson <carlos.robinson@zonesoftware.co>
2016-06-27 09:03:29 -0500

    searchbar and schedule

---
+++ MySampleApp/ListEventsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListEventsViewController.h
+//  Livi
+//
+//  Created by Carlos Robinson on 6/25/16.
+//  Copyright © 2016 livi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListEventsViewController : UIViewController
+
+@end
